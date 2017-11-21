61cb0862ddaf37cc329234d04d5cf80a58426e07
soni17 <elb004@gmail.com>
2017-11-21 11:27:20 -0500

    db migration to change column name from lattitude to latitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120223831) do
+ActiveRecord::Schema.define(version: 20171121161909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20171120223831) do
     t.text "content"
     t.string "source"
     t.string "image"
-    t.float "lattitude"
+    t.float "latitude"
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
