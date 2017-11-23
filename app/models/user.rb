3073e7115ee48bcfe72fa68d056be552ed318167
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-22 21:53:11 -0500

    added omniauthable to devise and modified the routes to overide the omniauth callback controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :stories
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 end
