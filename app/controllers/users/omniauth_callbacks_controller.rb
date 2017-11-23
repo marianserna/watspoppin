cf5da3167324fb04cda1d904240b539141d19233
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-22 23:05:09 -0500

    added comments to oauth controller to keep track of logic i am building, then added comment in the links partial for devise, then generated a services model where we will keep track of each provider, their tokens, any expiration and finally save the oauth hash for later use

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     p request.env['omniauth.auth']
-    
+    #look up existing user with this facebook account
+    #or we create a new user with this account
+
     redirect_to root_path
   end
 
