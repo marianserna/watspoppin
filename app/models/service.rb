9503b65e7c156c38c27afc02e676a4554872432b
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-23 15:51:29 -0500

    setup the koala to accept our api tokens and also instansiate the koala gem in the service model while passing in the access token

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ApplicationRecord
   belongs_to :user
+
+  def client
+    Koala::Facebook::API.new(access_token) #instansiate koala and pass in the access_token from the corresponding column in our model
+  end
 end
