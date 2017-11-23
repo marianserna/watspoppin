32040fa59dcccd7cc6fbdf8177fb4c87750e2b8c
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-22 23:06:17 -0500

    created a has_many association with the user model to services

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :stories
+  has_many :services
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
