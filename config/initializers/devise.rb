f10a6257ac8d12884c33824b899c499a210a852e
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-24 19:32:20 -0500

    configured devise to accept environment variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,6 +252,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'email, public_profile'
+   config.omniauth :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_SECRET_KEY"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
