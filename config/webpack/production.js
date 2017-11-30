e5e9f19fd5b8ea6753c57d24bc9bd436a08fa274
Marian Serna <marianhalliday@gmail.com>
2017-11-30 16:26:37 -0500

    Overriding uglify settings in webpack

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,25 @@
-const environment = require('./environment')
+const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig()
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const merge = require('webpack-merge');
+
+module.exports = merge({
+  customizeArray: merge.unique(
+    'plugins',
+    ['UglifyJsPlugin'],
+    plugin => plugin.constructor && plugin.constructor.name
+  )
+})(
+  {
+    plugins: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+          warnings: false,
+          comparisons: false // don't optimize comparisons
+        }
+      })
+    ]
+  },
+  environment.toWebpackConfig()
+);
