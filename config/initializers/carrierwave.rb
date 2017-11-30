1f4e33e578b0b129b12f78b1bb4a6972585603cb
Marian Serna <marianhalliday@gmail.com>
2017-11-30 11:41:46 -0500

    Configuring aws-fog

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      region:                'us-east-1'
+    }
+    config.fog_directory  = 'watspoppin'
+    config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
+  end
+end
