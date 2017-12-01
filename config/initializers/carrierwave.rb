f5bd7deb463e2232c8b7477173b1ed72855a2dce
Marian Serna <marianhalliday@gmail.com>
2017-12-01 09:48:15 -0500

    Changing S3 bucket directory

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       region:                'us-east-1'
     }
-    config.fog_directory  = 'watspoppin'
+    config.fog_directory  = 'watspoppinapp'
     config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
   end
 end
