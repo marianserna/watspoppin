47fb4e72e9c88dc92fa34b58eaef8d2266b6d5fb
soni17 <elb004@gmail.com>
2017-12-08 15:22:26 -0500

    testing require login for new and create actions

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -32,4 +32,25 @@ RSpec.describe StoriesController, type: :controller do
     end
   end
 
+  describe 'GET #new' do
+    it "requires login when clicking story button" do
+      get :new
+      expect(response).to redirect_to root_url
+    end
+
+    it 'renders new story view when user logged in' do
+      current_user = FactoryBot.attributes_for(:user)
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+  end
+
+  describe "POST #create" do
+    it "requires login when posting to stories#create" do
+      post :create, :params => { :story => FactoryBot.attributes_for(:story) }
+      expect(response).to redirect_to root_url
+    end
+  end
+
 end
