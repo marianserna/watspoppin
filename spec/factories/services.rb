1c1ccd5442ba3b6903479772b54bc53c7bde159f
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-07 23:04:33 -0500

    scaffolded facebook callback tests, added gems coderay for rspec syntax highlighting, included controllermacros module in rails helper and included the require 'devise', created a user factory, modified the services factory, controller macros now has the two mock hashes expected from each oauth client for testing and maps the environments for both devise and the omniauth clients

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :service do
-    user nil
-    provider "MyString"
-    uid "MyString"
-    access_token "MyString"
-    access_token_secret "MyString"
-    refresh_token "MyString"
+    user
+    provider "facebook"
+    uid "123456"
+    access_token "token"
+    access_token_secret "secret"
+    refresh_token "refresh"
     expires_at "2017-11-22 23:03:02"
     auth "MyText"
   end
