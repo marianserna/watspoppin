b57f64921d9c22405331b3b67c59212077342e03
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-10 18:08:41 -0500

    changed over the mailer to production and updated the env keys to pull in production for facebook. also updated the devise config to point to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,23 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # the default url that the email comes from
+  config.action_mailer.default_url_options = { host: 'https://watspoppinapp.herokuapp.com/' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
