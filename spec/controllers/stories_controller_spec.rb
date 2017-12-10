a757eaec94c9704128246f95d033769ebe1055c4
soni17 <elb004@gmail.com>
2017-12-10 17:29:19 -0500

    test story searching via hashtag

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -35,31 +35,27 @@ RSpec.describe StoriesController, type: :controller do
   fdescribe 'New Story' do
 
     context 'when user is not logged in' do
-
       it "redirects to root path" do
         get :new
         expect(response).to redirect_to root_url
       end
-
       it 'flashes a notice to log in' do
         get :new
         expect(flash[:notice]).to eq("Please log-in to create a story")
       end
-
       it "cannot post story" do
         post :create, :params => { :story => FactoryBot.attributes_for(:story) }
         expect(response).to redirect_to root_url
       end
-
     end
 
     context 'when user is logged in' do
       before(:each) do
         user = FactoryBot.create(:user)
         sign_in user
-          get :new
       end
       it 'renders new story view' do
+        get :new
         expect(response).to render_template(:new)
       end
       it 'if story saved, it redirects to root path' do
@@ -67,47 +63,39 @@ RSpec.describe StoriesController, type: :controller do
         post :create, :params => { :story => story }
         expect(response).to redirect_to root_path
       end
-
       it 'if story saved, it flashes message' do
         story = FactoryBot.attributes_for(:story)
         post :create, :params => { :story => story }
         expect(response).to redirect_to root_path
         expect(flash[:notice]).to eql("Story created")
       end
-
       it "if story doesn't save, it flashes message and renders the new template again" do
         story = {:content => nil, :image => nil, :latitude => nil, :longitude => nil}
         post :create, params: { story: story }
         expect(flash[:alert]).to  eq('Story could not be created. Please correct errors and try again.')
         expect(response).to render_template(:new)
       end
-
-
-
     end
 
   end
 
   fdescribe 'Hashtags' do
-    it 'when posting story, should extract hashtags and save in join table' do
+    before(:each) do
       user = FactoryBot.create(:user)
       sign_in user
       story = {:content => "this is a test story to test hashtags.  #test  #hashtagTest  #anotherTest", :image => nil, :latitude => nil, :longitude => nil}
       post :create, params: { story: story }
+    end
+    it 'when posting story, should extract hashtags and save in join table' do
       expect(Story.last.hashtags.length).to eql(3)
       expect(Story.last.hashtags[0].name).to eql('test')
       expect(Story.last.hashtags[1].name).to eql('hashtagTest')
       expect(Story.last.hashtags[2].name).to eql('anotherTest')
     end
-  end
-
-  fdescribe 'Geotags' do
-    it 'when posting story, it should extract geotags' do
-      user = FactoryBot.create(:user)
-      sign_in user
-      story = {:content => "this is a test story to test hashtags.  #test  #hashtagTest  #anotherTest", :image => nil, :latitude => nil, :longitude => nil}
+    it 'should be able to seach story via hashtag' do
+      testTag = Hashtag.find_by(name: "test")
+      expect(testTag.stories[0].content).to eql("this is a test story to test hashtags.  #test  #hashtagTest  #anotherTest")
     end
   end
-
 
 end
